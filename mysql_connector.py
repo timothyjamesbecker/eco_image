7647e3d94ab341030ab2b18ae066f9da5d242bdd
timothyjamesbecker <timothyjamesbecker@gmail.com>
2018-11-12 16:00:46 -0500

    db connection tests

--- mysql_connector.py
+++ mysql_connector.py
@@ -47,6 +47,7 @@ class MYSQL:
             print('uid: '),
             self.uid = sys.stdin.readline().replace('\n', '')
             self.pwd = getpass.getpass(prompt='pwd: ',stream=None)  # was stream=sys.sdin
+            print('password_internal is\n%s\n'%self.pwd)
         try:  # connection start
             conn = msc.connect(host=self.host, port=str(self.port), database=self.db, user=self.uid, password=self.pwd)
         except RuntimeError:
