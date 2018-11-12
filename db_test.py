095a8f141836d1e470c132717f97e311e031db2c
timothyjamesbecker <timothyjamesbecker@gmail.com>
2018-11-12 15:10:52 -0500

    db connection tests

---
+++ db_test.py
@@ -0,0 +1,36 @@
+import os
+import time
+import argparse
+import mysql_connector
+
+des="""eco image db connection and CRUD tester"""
+parser = argparse.ArgumentParser(description=des,formatter_class=argparse.RawTextHelpFormatter)
+parser.add_argument('-h', '--host',type=str, help='host name\t[None]')
+parser.add_argument('-p', '--port',type=int,help='port number\t[None]')
+parser.add_argument('-d', '--db',type=str,help='db schema name\t[None]')
+parser.add_argument('-t','--table',type=str,help='table name\t[None]')
+args = parser.parse_args()
+
+if not args.host is None:
+    host = args.host
+else:
+    raise AttributeError
+if not args.port is None:
+    port = args.port
+else:
+    raise AttributeError
+if not args.db is None:
+    db = args.db
+else:
+    raise AttributeError
+if not args.table is None:
+    tbl = args.table
+else:
+    tbl = 'test'
+
+with mysql_connector.MYSQL('%s:%s'%(host,port)) as dbo:
+    dbo.start()
+    SQL = 'select * from %s.%s;'%(db,tbl)
+    res = dbo.query(SQL,[],r=True)
+
+
