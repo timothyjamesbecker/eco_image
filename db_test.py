cfddb4907494237f7621dcc6e6a3cc5921f5255b
timothyjamesbecker <timothyjamesbecker@gmail.com>
2018-11-13 13:48:42 -0500

    db connection tests

--- db_test.py
+++ db_test.py
@@ -2,7 +2,7 @@
 import os
 import time
 import argparse
-import mysql.connector as msc
+import mysql_connector as msc
 
 des="""eco image db connection and CRUD tester"""
 parser = argparse.ArgumentParser(description=des,formatter_class=argparse.RawTextHelpFormatter)
@@ -35,12 +35,28 @@ if os.path.exists(local_path+'/flow.cfg'):
         raw = f.readlines()
         uid = raw[0].replace('\n','')
         pwd = raw[1].replace('\n','')
-sql,v = 'select * from %s.%s;'%(db,tbl),[]
-conn = msc.connect(host=unicode(host),port=unicode(port),database=unicode(db),user=unicode(uid),password=unicode(pwd))
-cursor = conn.cursor(dictionary=True)
-cursor.execute(sql,v)
-# for row in cursor: res.append(row)
-res = cursor.fetchall()
-cursor.close()
-conn.close()
-print(res)
+
+#wrapper library testing--------------------------------------------------------------------------------------------
+with msc.MSQL(host=host,port=port,db=db,uid=uid,pwd=pwd) as dbo:
+    C = """drop table if exists %s;
+           create table %s(pk int primary key not null, description text, lat float(10), lon float(10));"""%(tbl,tbl)
+    R = """select * from %s;"""%tbl
+    U =  """insert into %s values(13,"Needed",41.234526,-71.425362);
+            insert into %s values(21,"To Test",41.273652,72.162534);"""%(tbl,tbl)
+
+    D =  """truncate table %s;"""%tbl
+    Q = [C,R,U,D]
+    for q in Q:
+        res = dbo.query(q,[],r=True)
+        print(res)
+#--------------------------------------------------------------------------------------------------------------------
+
+#manual connector testing
+# conn = msc.connect(host=unicode(host),port=unicode(port),database=unicode(db),user=unicode(uid),password=unicode(pwd))
+# cursor = conn.cursor(dictionary=True)
+# cursor.execute(sql,v)
+# # for row in cursor: res.append(row)
+# res = cursor.fetchall()
+# cursor.close()
+# conn.close()
+# print(res)
