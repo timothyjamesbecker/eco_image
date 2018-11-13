dae4df198acd5c0fbc254a91b4c225dd02879da5
timothyjamesbecker <timothyjamesbecker@gmail.com>
2018-11-13 17:16:45 -0500

    db connection tests

--- db_test.py
+++ db_test.py
@@ -35,17 +35,15 @@ if os.path.exists(local_path+'/flow.cfg'):
         raw = f.readlines()
         uid = raw[0].replace('\n','')
         pwd = raw[1].replace('\n','')
-
 #wrapper library testing--------------------------------------------------------------------------------------------
 with msc.MYSQL(host=host,port=port,db=db,uid=uid,pwd=pwd) as dbo:
     C = """drop table if exists %s;
            create table %s(pk int primary key not null, description text, lat float(10), lon float(10));"""%(tbl,tbl)
-    R = """select * from %s;"""%tbl
+    R = """select * from %s.%s;"""%(db,tbl)
     U =  """insert into %s values(13,"Needed",41.234526,-71.425362);
             insert into %s values(21,"To Test",41.273652,72.162534);"""%(tbl,tbl)
-
     D =  """truncate table %s;"""%tbl
-    Q = [R,D,R,C,U,R]
+    Q = [R]
     for q in Q:
         res = dbo.query(q.replace(' ','').replace('\n',''),[],r=True)
         print(res)
