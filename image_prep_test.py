c2dfaf7fddb64fe90c048509838edc16d069d15c
timothyjamesbecker <timothyjamesbecker@gmail.com>
2018-12-05 11:04:33 -0500

    auto segmentation of pic footers

--- image_prep_test.py
+++ image_prep_test.py
@@ -10,7 +10,7 @@ local_path = os.path.dirname(os.path.abspath(__file__))
 img = cv2.imread(local_path+'/data/camera_examples/M-999i/15100_SpruceSwampCreek_051018_062018 (1).JPG')
 img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-edges = cv2.Canny(img,25,50,apertureSize=3)
+edges = cv2.Canny(img,0,25,apertureSize=3)
 lines = cv2.HoughLines(edges,1,np.pi/180,200)
 img = cv2.cvtColor(img_gray,cv2.COLOR_GRAY2BGR)
 for rho,theta in lines[0]:
@@ -18,14 +18,12 @@ for rho,theta in lines[0]:
     b = np.sin(theta)
     x0 = a*rho
     y0 = b*rho
-    x1 = int(x0 + img.shape[0]*(-b))
-    y1 = int(y0 + img.shape[1]*(a))
-    x2 = int(x0 - img.shape[0]*(-b))
-    y2 = int(y0 - img.shape[1]*(a))
+    x1 = int(x0 + img.shape[1]*(-b))
+    y1 = int(y0 + img.shape[0]*(a))
+    x2 = int(x0 - img.shape[1]*(-b))
+    y2 = int(y0 - img.shape[0]*(a))
     cv2.line(img,(x1,y1),(x2,y2),(0,0,255),40)
-    # cv2.circle(img,(x1,y1),100,(0,0,255))
     print((x1,y1,x2,y2))
-cv2.circle(img,(img.shape[0]/2,img.shape[1]/2),100,(0,0,255),20)
 plt.imshow(cv2.cvtColor(img,cv2.COLOR_BGR2RGB))
 plt.show()
 
