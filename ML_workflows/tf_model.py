b04cd170604b11b45da0887718942dd1d8e41d18
timothyjamesbecker <timothyjamesbecker@gmail.com>
2019-10-21 21:22:23 -0400

    starting keras based tf_model test script

--- ML_workflows/tf_model.py
+++ ML_workflows/tf_model.py
@@ -1,9 +1,53 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
+import numpy as np
 import tensorflow as tf
 from tensorflow import keras
-import numpy as np
-import matplotlib.pyplot as plt
-print(tf.__version__)
+import utils
 
-fashion_mnist = keras.datasets.fashion_mnist
-(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+#BASIC CONNECTED NN------------------------------------------------------------
+for label in [1,2,3,4,5,6]:
+    # in_dir = '/media/data/flow_2018_small_gray/'
+    # (train_images,train_labels),(test_images,test_labels) = utils.load_data_advanced(in_dir)
+    # train_images  = train_images/255.0
+    # train_labels  = np.asarray([1 if i==label else 0 for i in train_labels],dtype='uint8')
+    # test_images   = test_images/255.0
+    # test_labels   = np.asarray([1 if i==label else 0 for i in test_labels],dtype='uint8')
+    #
+    # model = keras.Sequential([
+    #     keras.layers.Flatten(input_shape=train_images.shape[1:]),
+    #     keras.layers.Dense(128, activation='relu'),
+    #     keras.layers.Dense(2, activation='softmax')
+    # ])
+    # model.compile(optimizer='adam',
+    #               loss='sparse_categorical_crossentropy',
+    #               metrics=['accuracy'])
+    # model.fit(train_images,train_labels,epochs=10,verbose=0)
+    # test_loss, test_acc = model.evaluate(test_images,test_labels,verbose=0)
+    # print('%s MLP accuracy for binary classifier %s'%(test_acc,label))
+
+    #Deep CNN -------------------------------------------------------------
+    in_dir = '/media/data/flow_2018_medium_color/'
+    (train_images,train_labels),(test_images,test_labels) = utils.load_data_advanced(in_dir,gray_scale=False)
+    train_images  = train_images/255.0
+    train_labels  = np.asarray([1 if i==label else 0 for i in train_labels],dtype='uint8')
+    test_images   = test_images/255.0
+    test_labels   = np.asarray([1 if i==label else 0 for i in test_labels],dtype='uint8')
+
+    model = keras.Sequential([
+        keras.layers.Conv2D(64,(3,3), activation='relu',input_shape=train_images.shape[1:]),
+        keras.layers.MaxPooling2D((2,2)),
+        keras.layers.Conv2D(128, (3,3), activation='relu'),
+        keras.layers.MaxPooling2D((2,2)),
+        keras.layers.Conv2D(128, (3,3), activation='relu'),
+        keras.layers.MaxPooling2D((2, 2)),
+        keras.layers.Conv2D(64, (3, 3), activation='relu'),
+        keras.layers.Flatten(),
+        keras.layers.Dense(64, activation='relu'),
+        keras.layers.Dense(2, activation='softmax')
+    ])
+    model.compile(optimizer='adam',
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+    model.fit(train_images,train_labels,epochs=10,verbose=0)
+    test_loss, test_acc = model.evaluate(test_images,test_labels,verbose=0)
+    print('%s CNN accuracy for binary classifier %s '%(test_acc,label))
--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ import sys
 import exifread
 import piexif
 import cv2
+import glob
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -77,15 +78,16 @@ def resize(img,width=640,height=480,interp=cv2.INTER_CUBIC):
     w_scale =  width/(img.shape[1]*1.0)
     if w_scale<h_scale:
         dim = (int(round(img.shape[1]*h_scale)),int(round(img.shape[0]*h_scale)))
-        img = cv2.resize(img,dim)
+        img = cv2.resize(img,dim,interpolation=interp)
         d = int(round((img.shape[1]-width)/2.0))
         img = img[:,d:(img.shape[1]-d),:]
     else:
         dim = (int(round(img.shape[1]*w_scale)),
                int(round(img.shape[0]*w_scale)))
-        img = cv2.resize(img,dim)
+        img = cv2.resize(img,dim,interpolation=interp)
         d = int(round((img.shape[0]-height)))
         img = img[d:img.shape[0],:,:]
+    img = cv2.resize(img,(width,height),interpolation=interp)
     return img
 
 def plot(img):
@@ -100,3 +102,85 @@ def img2blob(img):
 
 def imgs2pickle(imgs):
     return True
+
+def load_data_simple(in_dir,gray_scale=True,split=0.15):
+    data,labels = [],[]
+    if gray_scale:
+        for path in sorted(glob.glob(in_dir+'/*/*.jpg')+glob.glob(in_dir+'/*/*.JPG')):
+            labels += [int(path.rsplit('label_')[-1].rsplit('/')[0])]
+            data   += [cv2.imread(path,cv2.IMREAD_GRAYSCALE)]
+        l,(h,w) = int(round(split*len(data))),data[0].shape
+        test_idx  = np.asarray(sorted(np.random.choice(range(len(data)),l,replace=False)))
+        train_idx = np.asarray(sorted(list(set(range(len(data))).difference(set(list(test_idx))))))
+        test_labels  = np.asarray(labels, dtype='uint8')[test_idx]
+        train_labels = np.asarray(labels, dtype='uint8')[train_idx]
+        test_data    = np.ndarray(shape=(len(test_idx),h,w),dtype='uint8')
+        train_data   = np.ndarray(shape=(len(train_idx),h,w),dtype='uint8')
+        for i in range(len(test_data)): test_data[i,:,:]  = data[test_idx[i]]
+        for i in range(len(train_data)):train_data[i,:,:] = data[train_idx[i]]
+    else:
+        for path in sorted(glob.glob(in_dir+'/*/*.jpg')+glob.glob(in_dir+'/*/*.JPG')):
+            labels += [int(path.rsplit('label_')[-1].rsplit('/')[0])]
+            data   += [cv2.imread(path)]
+        l,(h,w,c) = int(round(split*len(data))),data[0].shape
+        test_idx     = np.asarray(sorted(np.random.choice(range(len(data)),l,replace=False)))
+        train_idx    = np.asarray(sorted(list(set(range(len(data))).difference(set(list(test_idx))))))
+        test_labels  = np.asarray(labels,dtype='uint8')[test_idx]
+        train_labels = np.asarray(labels,dtype='uint8')[train_idx]
+        test_data    = np.ndarray(shape=(len(test_idx),h,w,c),dtype='uint8')
+        train_data   = np.ndarray(shape=(len(train_idx),h,w,c),dtype='uint8')
+        for i in range(len(test_data)):
+            test_data[i,:,:,:] = data[test_idx[i]]
+        for i in range(len(train_data)):
+            train_data[i,:,:,:] = data[train_idx[i]]
+    return (train_data,train_labels),(test_data,test_labels)
+
+#advanced data loader splits with at least one site that is unseen...
+#must have a minimum of two sites per label in order to train/test
+def load_data_advanced(in_dir,gray_scale=True,split=0.15,verbose=True):
+    L = {}
+    paths = sorted(glob.glob(in_dir+'/*/*.jpg')+glob.glob(in_dir+'/*/*.JPG'))
+    for i in range(len(paths)):
+        sid   = int(paths[i].rsplit('/')[-1].rsplit('_')[0])
+        label = int(paths[i].rsplit('label_')[-1].rsplit('/')[0])
+        if sid in L: L[sid] += [[i,label]]
+        else:        L[sid]  = [[i,label]]
+    trn_data,trn_labels,tst_data,tst_labels = [],[],[],[]
+    ts   = max(1,round(len(L)*split))
+    sids = list(L.keys())
+    test_sidx  = sorted(list(np.random.choice(sids,ts)))
+    train_sidx = sorted(list(set(sids).difference(set(test_sidx))))
+    if verbose: print('sids %s selected for test cases'%(','.join([str(x) for x in test_sidx])))
+    if gray_scale:
+        for sid in test_sidx:
+            for [i,label] in L[sid]:
+                tst_labels  += [label]
+                tst_data    += [cv2.imread(paths[i],cv2.IMREAD_GRAYSCALE)]
+        for sid in train_sidx:
+            for [i,label] in L[sid]:
+                trn_labels += [label]
+                trn_data   += [cv2.imread(paths[i],cv2.IMREAD_GRAYSCALE)]
+        (h,w) = trn_data[0].shape
+        train_labels = np.asarray(trn_labels,dtype='uint8')
+        test_labels  = np.asarray(tst_labels,dtype='uint8')
+        train_data   = np.ndarray((len(trn_data),h,w),dtype='uint8')
+        test_data    = np.ndarray((len(tst_data),h,w),dtype='uint8')
+        for i in range(len(train_data)): train_data[i,:,:] = trn_data[i]
+        for i in range(len(test_data)):  test_data[i,:,:]  = tst_data[i]
+    else:
+        for sid in test_sidx:
+            for [i,label] in L[sid]:
+                tst_labels  += [label]
+                tst_data    += [cv2.imread(paths[i])]
+        for sid in train_sidx:
+            for [i,label] in L[sid]:
+                trn_labels += [label]
+                trn_data   += [cv2.imread(paths[i])]
+        (h,w,c) = trn_data[0].shape
+        train_labels = np.asarray(trn_labels,dtype='uint8')
+        test_labels  = np.asarray(tst_labels,dtype='uint8')
+        train_data   = np.ndarray((len(trn_data),h,w,c),dtype='uint8')
+        test_data    = np.ndarray((len(tst_data),h,w,c),dtype='uint8')
+        for i in range(len(train_data)): train_data[i,:,:,:] = trn_data[i]
+        for i in range(len(test_data)):  test_data[i,:,:,:]  = tst_data[i]
+    return (train_data,train_labels),(test_data,test_labels)
